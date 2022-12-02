bff2ee77c08cde6dabf26cd5e9e45235ab28ab58
hoang19020302 <91183884+hoang19020302@users.noreply.github.com>
2022-12-02 15:24:37 +0700

    Update connections.py

--- connections.py
+++ connections.py
@@ -1,13 +1,32 @@
-import asyncio
-import websockets
-import struct
+import json
+import socket
+msg = {"result": 1, "ip": "localhost", "port": 10010, "path": "path"}
 
-a = [1,9,b'localhost',27003,5,b'bingo',7,b'doan_so',4,b'nh08']
-pack=struct.pack(f'ii{a[1]}s',a[0],a[1],a[2])+struct.pack(f'ii{a[4]}s',a[3],a[4],a[5])+struct.pack(f'i{a[6]}s',a[6],a[7])+struct.pack(f'i{a[8]}s',a[8],a[9])
-async def hello():
-    async with websockets.connect("ws://104.194.240.16:8881") as websocket:
-        await websocket.send(pack)
-        msg = await websocket.recv()
-        print(msg)
-asyncio.run(hello())
-print(pack)
+def server_program():
+    # get the hostname
+    host = 'localhost'
+    # initiate port no above 1024
+    port = 8881
+
+    server_socket = socket.socket() # get instance
+    # look closely, the bind() function takes tuple as argument
+    server_socket.bind((host, port)) # bind host address and port together
+
+    # configure how many client the server can listen simultaneously
+    server_socket.listen(2)
+    conn, address = server_socket.accept() # accept new connection
+    print("Connection from: " + str(address))
+    while True:
+        # receive data stream. it won't accept data packet greater than 1024 bytes
+        data = conn.recv(1024).decode()
+        if not data:
+            # if data is not received break
+            break
+        print("form connected user: " + str(data))
+        # data = input(' -> ')
+        conn.send(json.dumps(msg).encode()) # send data to the client
+
+    conn.close() # close the connection
+
+if __name__ == '__main__':
+    server_program()
