6ba6a90a8d053125fca774108f4f3dac58c01517
hoang19020302 <91183884+hoang19020302@users.noreply.github.com>
2022-12-06 16:08:05 +0700

    Update status.py

--- status.py
+++ status.py
@@ -3,17 +3,36 @@ import websockets
 import struct
 import json
 
-data = {
+# cập nhập trạng thái
+data1 = {
     "result": 2,
-    "match": 8, # id của ván game
-    "status": 1,
-    "id1": 1000,
+    "match": 63, # id của match
+    "status": 2, # trạng thái match
+    "id1": 100,
     "id2": 100
+}
+
+# thông báo bắt đầu
+data2 = {
+    "result": 1,
+    "match": 63, # id của match
+}
+
+# thông báo kết thúc
+data3 = {
+    "result": 3,
+    "match": 63, # id của match
+}
+
+# thông báo lỗi
+data4 = {
+    "result": 0,
+    "match": 63, # id của match
 }
 
 async def hello():
     async with websockets.connect("ws://104.194.240.16/ws/channels/") as websocket:
-        await websocket.send(json.dumps(data))
+        await websocket.send(json.dumps(data1))
         msg = await websocket.recv()
         print(msg)
 asyncio.run(hello())
